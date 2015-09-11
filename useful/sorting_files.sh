ff9a53ae2c723080b17a5afd37da8b2359f4abd8
ABiatov <anton.biatov@gmail.com>
2015-09-11 05:33:43 +0300

    Create sorting_files.sh

---
+++ useful/sorting_files.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+mkdir jpg
+mkdir JPG_nikon
+mkdir png
+mkdir bmp
+mkdir pdf
+mkdir djvu
+mkdir doc
+mkdir rtf
+mkdir xls
+mkdir torrent
+mkdir avi
+mkdir flv
+mkdir mp4
+mkdir mp3
+mkdir arhives
+mkdir exe
+mv ./*.jpg ./jpg/
+mv ./*.JPG ./JPG_nikon/
+mv ./*.png ./png/
+mv ./*.bmp ./bmp/
+mv ./*.BMP ./bmp/
+mv ./*.pdf ./pdf/
+mv ./*.PDF ./pdf/
+mv ./*.djvu ./djvu/
+mv ./*.doc ./doc/
+mv ./*.docx ./doc/
+mv ./*.rtf ./rtf/
+mv ./*.xls ./xls/
+mv ./*.xlsx ./xls/
+mv ./*.torrent ./torrent/
+mv ./*.avi ./avi/
+mv ./*.flv ./flv/
+mv ./*.mp4 ./mp4/
+mv ./*.mp3 ./mp3/
+mv ./*.exe ./exe/
+mv ./*.zip ./arhives/
+mv ./*.rar ./arhives/
+mv ./*.7z ./arhives/
+mv ./*.gz ./arhives/
