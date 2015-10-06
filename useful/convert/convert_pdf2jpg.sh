493e94b2a208e00c21c6f5e96f64eb5ab1caae4b
Anton Biatov <anton.biatov@gmail.com>
2015-10-06 14:41:11 +1000

    Update convert_pdf2jpg.sh

--- useful/convert/convert_pdf2jpg.sh
+++ useful/convert/convert_pdf2jpg.sh
@@ -2,11 +2,11 @@
 # chmod +x ./convert_pdf2jpg.sh
 # https://gis-naturalist.blogspot.com/
 
-# Конвертирует pdf-файл в изображения jpg в 90% сжатием постранично 
+# Конвертирует pdf-файл в изображения jpg в 100% качеством постранично 
 # в новую папку, которую называет по имени исходного pdf-файла.
 
 for fname1 in *.pdf
 do fname2="$(basename $fname1 .pdf)"
 mkdir ./$fname2
-convert $fname1 -compress JPEG -quality 90% ./$fname2/$fname2.jpg
+convert $fname1 -compress JPEG -quality 100% ./$fname2/$fname2.jpg
 done
