a2713ec75cf103cf0c82a9e65912828e66dd1a19
Anton Biatov <anton.biatov@gmail.com>
2015-10-06 14:40:30 +1000

    Create convert_pdf2png.sh

---
+++ useful/convert/convert_pdf2png.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# chmod +x ./convert_pdf2png.sh
+# https://gis-naturalist.blogspot.com/
+# Конвертирует pdf-файл в изображения png постранично 
+# в новую папку, которую называет по имени исходного pdf-файла.
+
+for fname1 in *.pdf
+do fname2="$(basename $fname1 .pdf)"
+mkdir ./$fname2
+convert $fname1 ./$fname2/$fname2.png
+done
+
