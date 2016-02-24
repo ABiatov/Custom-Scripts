4353528080ed0330be114f91c144e96dd326d0a8
Anton Biatov <anton.biatov@gmail.com>
2016-02-25 04:56:25 +0900

    Update tif2asc.r

--- GIS/Raster/tif2asc/tif2asc.r
+++ GIS/Raster/tif2asc/tif2asc.r
@@ -1,6 +1,5 @@
 #!/usr/bin/env Rscript
 
 library("raster")
-# setwd("/home/narwhale/Maps/WD/Diplom_Alin-wild_boar/TIF/")
 rrrrr <- raster("temp.tif", package="raster")
 writeRaster(rrrrr, filename="rrrrr.asc", format = "ascii", datatype='FLT4S', overwrite=TRUE)
