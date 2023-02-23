23f6e2ba663d79c53826554277aa4c50090f678b
Anton Biatov <anton.biatov@gmail.com>
2023-02-23 22:56:47 +0200

    update horizontal legend generator

--- Python/Draw_horizontal_gradient_color_legend_for_map/picture_generator_v1.py
+++ Python/Draw_horizontal_gradient_color_legend_for_map/picture_generator_v1.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 # Define the dimensions of the rectangle
 width = 1500
-height = 40
+height = 45
 
 # Define the list of colors
 list_colors = ['43 131 186', '171 221 164', '255 255 191', '253 174 97', '215 25 28']
@@ -13,8 +13,6 @@ label_list = [0, 10, 20, 30, 40, 50]
 # Convert the list of colors from strings to tuples of integers
 list_colors = [tuple(map(int, color.split())) for color in list_colors]
 
-
-
 # Create a new image with the same dimensions as the rectangle
 image = Image.new("RGB", (width, height), color="white")
 
@@ -22,7 +20,7 @@ image = Image.new("RGB", (width, height), color="white")
 draw = ImageDraw.Draw(image)
 
 # Load a font file and create a font object
-font = ImageFont.truetype("arial.ttf", 16)
+font = ImageFont.truetype("Roboto-Medium.ttf", 20)
 
 # Draw the gradient filled rectangle
 for x in range(width):
@@ -36,30 +34,18 @@ for x in range(width):
     # Draw a vertical line of this color at this column
     draw.line((x, 0, x, 15), fill=color)
 
-    # Check if this column corresponds to a label
-    if x * (len(label_list) - 1) % width == 0:
-        # Get the label text
-        label_text = str(label_list[x * (len(label_list) - 1) // width])
+for i, label in enumerate(label_list):
+    label_text = str(label)
+    label_size = draw.textsize(label_text)
+    label_y = height - label_size[1] - 15
+    if i == 0:
+        label_x = 1
+    elif i == len(label_list) - 1:
+        label_x = width - label_size[0] - 11 # todo calculate and set position for last label on independets from numbers o digits
+    else:
+        label_x = int(i * (width - label_size[0]) / (len(label_list) - 1))
 
-        # Get the size of the label text
-        label_size = font.getsize(label_text)
-
-        # Calculate the position to draw the label text
-        # label_x = x - label_size[0] // 2
-        label_y = height - label_size[1] - 5
-
-        print("x: ", x)
-
-        # Calculate the position to draw the label text
-        if x == 0:
-            label_x = 1
-        elif x == width + 300: # todo calculate and set position for last label
-            label_x = width - 10
-        else:
-            label_x = x - label_size[0] // 2
-
-        # Draw the label text
-        draw.text((label_x, label_y), label_text, font=font, fill="black")
+    draw.text((label_x, label_y), label_text, font=font, fill="black")
 
 # Save the image to a file
-image.save("gradient_rectangle_horizontal_labels.png")
+image.save("horizontal_gradient_labels.png")
